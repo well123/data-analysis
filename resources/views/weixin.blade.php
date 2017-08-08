88cc8313f0813dbc878c2cfe0f2112fdd4563830
武斌 <wubin@wubindeMacBook-Pro.local>
2017-08-08 10:15:13 +0800

    dd

--- resources/views/weixin.blade.php
+++ resources/views/weixin.blade.php
@@ -1,4 +1,14 @@
-<div id="weixindialog"></div>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>testdetails </title>
+    <link rel="stylesheet" href="">
+    <script src="https://unpkg.com/vue/dist/vue.js"></script>
+    <script src="http://res.wx.qq.com/connect/zh_CN/htmledition/js/wxLogin.js"></script>
+</head>
+<body>
+<div id="testss">
+<div id="weixindialog"></div></div></body>
 <script>
     var app = new Vue({
         el:"#testss",
@@ -15,7 +25,7 @@
 
                 scope: "snsapi_login",
 
-                redirect_uri: "http://www.baidu.com",
+                redirect_uri: "http://www.haitun.hk",
 
                 state: "243",
 
